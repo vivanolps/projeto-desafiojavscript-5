dbc57ba99f06209213833557458b0b30feac843a
Evelyn Lima <vinhahh.3000@gmail.com>
2025-05-26 09:16:40 -0300

    Coloquei o carrosel, mudei o width e o heigth do css, mas falta ajustar a imagem no navegador

--- js/carousel.js
+++ js/carousel.js
@@ -1,32 +1,51 @@
-
-
-//carousel
-
-//Array storage class
-let carouselArr = [];
-
-
-//class Carousel
 class Carousel {
+    constructor(image, title, url) {
+        this.image = image;
+        this.title = title;
+        this.url = url;
+    }
 
-    
-      
-    static Start(arr){
-        if(arr){
+    static Start(arr) {
+        if (arr && arr.length > 0) {
+            Carousel._carouselArr = arr;
+            Carousel._sequence = 0;
+            Carousel._size = arr.length;
+            Carousel.Next();
 
-            if(arr.length > 0){
-                Carousel._sequence = 0;
-                Carousel._size = arr.length;
-                Carousel.Next(); //start
-                Carousel._interval = setInterval(function(){ Carousel.Next(); },5000);
-            }
-            
+            document.getElementById('prev-bnt').addEventListener('click', Carousel.Previous);
+            document.getElementById('next-bnt').addEventListener('click', Carousel.Next);
         } else {
-            throw "Method Start need a Array Variable.";
+            throw "O método Start precisa de um array";
         }
     }
 
-    static Next(){
-        
+    static Previous() {
+        Carousel._sequence = (Carousel._sequence - 1 + Carousel._size) % Carousel._size;
+        const item = Carousel._carouselArr[Carousel._sequence];
+        Carousel.Show(item);
+}
+
+static Next() {
+    Carousel._sequence = (Carousel._sequence + 1) % Carousel._size;
+    const item = Carousel._carouselArr[Carousel._sequence];
+    Carousel.Show(item);
+}
+
+static Show(item) {
+    const divImage = document.getElementById('carousel');
+    const divTitle = document.getElementById('carousel-title');
+
+    divImage.innerHTML = `<img src="img/${item.image}" style="width:100%; max-height:400px; object-fit:cover;">`;
+    divTitle.innerHTML = `<a href="${item.url}" style="text-decoration:none; color:#1351d8; font-size:20px;">${item.title}</a>`;
     }
-};
+}
+
+//agora vou definir os slides
+const carouselArr = [];
+carouselArr.push(new Carousel("imagem_1.jpg", "Esta é a nova Ranger Ford 2022. Verifique novidades.", "lancamento.html"));
+carouselArr.push(new Carousel("imagem_2.jpg", "Ford a nossa história", "#"));
+carouselArr.push(new Carousel("imagem_3.jpg", "Nova Ford Bronco Sport 2022", "lancamento.html"));
+
+window.addEventListener('DOMContentLoaded', () => {
+    Carousel.Start(carouselArr);
+});
