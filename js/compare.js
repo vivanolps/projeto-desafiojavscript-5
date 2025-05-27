df7142c815316c2977564abe29b3e556c40d0959
Evelyn Lima <vinhahh.3000@gmail.com>
2025-05-27 09:13:53 -0300

    mudando estruturas no javascript

--- js/compare.js
+++ js/compare.js
@@ -82,7 +82,7 @@ function UpdateCompareTable() {
         
         document.getElementById("compare_volumecacamba_" + i).textContent = carArr[i].volumeCacamba + "L";
         
-        document.getElementById("compare_roda_" + 1).textContent = carArr[i].roda;
+        document.getElementById("compare_roda_" + i).textContent = carArr[i].roda;
 
         document.getElementById("compare_preco_" + i).textContent = "R$ " + carArr[i].preco.toLocaleString("pt-BR", {minimumFractionDigits: 2}); // ajuste porque na tabela o preço estava parecendo errado
     }
